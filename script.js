f74ccfe0096e8f132d658ead7bac2dbf106a4260
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-19 17:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age =age;
+	}
+	get name(){
+		return this.name;
+	}
 
-class Student extends Person {}
+	set age(age){
+		this.age = age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	constructor(name, age){
+		super(name, age)
+		
+	}
+	study(){
+		console.log(this.name+" is studying")
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		super(name, age)
+		
+	}
+	teach(){
+		console.log(this.name+" is teaching")
+	}
+}
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
